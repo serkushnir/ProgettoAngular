2bd39beee45cbe97929d096e3e111971af0a7331
serkushnir <113978626+serkushnir@users.noreply.github.com>
2024-05-13 19:29:47 +0000

    components creation

---
+++ Progetto/src/app/statistiche/statistiche.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistiche',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistiche.component.html',
+  styleUrl: './statistiche.component.css'
+})
+export class StatisticheComponent {
+
+}
---
+++ Progetto/src/app/tabella-regioni/tabella-regioni.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabella-regioni',
+  standalone: true,
+  imports: [],
+  templateUrl: './tabella-regioni.component.html',
+  styleUrl: './tabella-regioni.component.css'
+})
+export class TabellaRegioniComponent {
+
+}
