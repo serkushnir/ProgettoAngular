e230f60be0bcba7e729fb5eebfb1aa90ac0f1b7e
serkushnir <113978626+serkushnir@users.noreply.github.com>
2024-05-13 20:01:37 +0000

    login

--- Progetto/src/app/login/login.component.ts
+++ Progetto/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  mail: string = "";
+  password: string = "";
+
+  constructor(private auth: AuthService) { }
+
+  //metodo per effettuare il login
+  login() {
+    if (this.mail == "" || this.password == "") {
+      alert("Inserire tutti i campi!");
+      return;
+    }
+
+    this.auth.login(this.mail, this.password);
+
+    this.mail = "";
+    this.password = "";
+  }
 
 }
