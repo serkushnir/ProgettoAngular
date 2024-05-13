0ec11b1ca1a87f3d9b6e738490830c59c4e3b297
serkushnir <113978626+serkushnir@users.noreply.github.com>
2024-05-13 19:48:14 +0000

    database

--- Progetto/src/app/service/api.service.ts
+++ Progetto/src/app/service/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,38 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  //variabile per lo stile della navbar
+  arrayPosizione: string[] = ["stileSelezionato", "stileDefault", "stileDefault"];
+
+  constructor(public http: HttpClient) { }
+
+  //ultimi dati nazionali
+  public getDataNazionale(){
+    return this.http.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json');
+  }
+
+  //ultimi dati regionali
+  public getDataRegioni(){
+    return this.http.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json');
+  }
+
+  //tutti i dati regionali
+  public getDataTotaleNazione(){
+    return this.http.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json');
+  }
+
+  //ultimi dati regionali
+  public getDataRegioniLatest(){
+    return this.http.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json');
+  }
+
+//metodo per cambiare lo stile della navbar
+cambiaStile(posizione: number)
+{
+  for(let i = 0; i < this.arrayPosizione.length; i++)
+  {
+    this.arrayPosizione[i] = "stileDefault";
+  }
+  this.arrayPosizione[posizione] = "stileSelezionato";
+}
 }
